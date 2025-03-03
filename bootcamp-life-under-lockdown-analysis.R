d92a57d19299f892a7907519a98f2f4016281dd1
Sophie Che <alinche@192.168.1.113>
2025-03-03 16:34:02 +0000

    show the mean of beliefs at each level of know_facebook

--- bootcamp-life-under-lockdown-analysis.R
+++ bootcamp-life-under-lockdown-analysis.R
@@ -3,4 +3,15 @@ library(readr)
 
 bc_ld <- read_csv('../data/life-under-lockdown-wave2-extract.csv')
 
+bc_ld %>%
+  mutate(know_facebook = ordered(
+    know_facebook,
+    levels = c(
+      'Nothing at all',
+      'Not very much',
+      'A fair amount',
+      'A great deal'
+    ))) %>%
+  group_by(know_facebook) %>%
+  summarise(n = n(), Beliefs = mean(beliefs))
 
